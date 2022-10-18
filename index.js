2014099554c25bb681254875132eadb9a697a7f5
Scott Le <tle@ipipeline.com>
2022-10-18 13:48:39 -0700

    new file

---
+++ index.js
@@ -0,0 +1,9 @@
+# from GitHub sampleform 
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
